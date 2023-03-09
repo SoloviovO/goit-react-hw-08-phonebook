07c9ac86ddc0ddaf5b694e0805e7dd8b4aed6d06
SoloviovO <soloviov.o.m@gmail.com>
2023-03-09 23:52:53 +0200

    add total of contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,17 @@
 import { ContactItem } from 'components/ContactItem/ContactItem';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectFilteredContacts } from 'redux/contacts/contactsSelectors';
+import {
+  selectAllContacts,
+  selectFilteredContacts,
+} from 'redux/contacts/contactsSelectors';
 import { fetchContacts } from 'redux/contacts/contactsThunk';
 import { Box, Container, List, Text } from '@chakra-ui/react';
 
 export const ContactList = () => {
   const filteredContacts = useSelector(selectFilteredContacts);
   const dispatch = useDispatch();
+  const totalContacts = useSelector(selectAllContacts);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -16,6 +20,12 @@ export const ContactList = () => {
   return (
     <Container maxW="container.xl">
       <Box maxW="450px" m="0 auto" p="0 50px">
+        {totalContacts.length > 0 && (
+          <Text textAlign="center" fontWeight="500" mb="10px">
+            You generally have {totalContacts.length}&nbsp;
+            {totalContacts.length === 1 ? 'contact' : 'contacts'}
+          </Text>
+        )}
         <List>
           {filteredContacts.length > 0 ? (
             filteredContacts.map(({ id, name, number }) => (
--- src/redux/contacts/contactsSelectors.js
+++ src/redux/contacts/contactsSelectors.js
@@ -12,3 +12,5 @@ export const selectFilteredContacts = createSelector(
     );
   }
 );
+
+export const selectAllContacts = state => state.contacts.contacts.items;
