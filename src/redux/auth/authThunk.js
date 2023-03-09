13cac6f01fa75bea1a742d3b12110fa84ef651f9
SoloviovO <soloviov.o.m@gmail.com>
2023-03-10 00:13:13 +0200

    done

--- src/redux/auth/authThunk.js
+++ src/redux/auth/authThunk.js
@@ -14,29 +14,41 @@ const token = {
 
 export const registerUser = createAsyncThunk(
   'auth/register',
-  async credential => {
+  async (credential, thunkAPI) => {
     try {
       const { data } = await axios.post('/users/signup', credential);
       token.set(data.token);
       return data;
-    } catch (error) {}
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
-export const logInUser = createAsyncThunk('auth/login', async credential => {
-  try {
-    const { data } = await axios.post('/users/login', credential);
-    token.set(data.token);
-    return data;
-  } catch (error) {}
-});
+export const logInUser = createAsyncThunk(
+  'auth/login',
+  async (credential, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/users/login', credential);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-export const logOutUser = createAsyncThunk('auth/logout', async () => {
-  try {
-    await axios.post('/users/logout');
-    token.unset();
-  } catch (error) {}
-});
+export const logOutUser = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkAPI) => {
+    try {
+      await axios.post('/users/logout');
+      token.unset();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
 export const fetchCurrentUser = createAsyncThunk(
   'auth/refresh',
@@ -51,6 +63,8 @@ export const fetchCurrentUser = createAsyncThunk(
     try {
       const { data } = await axios.get('/users/current');
       return data;
-    } catch (error) {}
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
